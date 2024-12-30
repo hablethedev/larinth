b43aa1d5dccd449c08b07133f77fb7a0b160427b
Hazle <xendyex@tutanota.com>
2024-12-30 20:18:58 +0000

    Setup: Remove Unneeded Stuff
    Launcher/app only project :3

--- apps/labrinth/src/models/error.rs
+++
@@ -1,8 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-/// An error returned by the API
-#[derive(Serialize, Deserialize)]
-pub struct ApiError<'a> {
-    pub error: &'a str,
-    pub description: String,
-}
--- apps/labrinth/src/queue/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod analytics;
-pub mod maxmind;
-pub mod moderation;
-pub mod payouts;
-pub mod session;
-pub mod socket;
